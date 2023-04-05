21139721302e5a48d63577dd5fcc699c6cef4064
sabrina <sabrinaealvarez@gmail.com>
2023-04-05 01:32:35 -0300

    added Procfile

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import { schema } from "./schema.js";
 import { dataSources } from "./dataSource.js";
 
 const server = new ApolloServer({ schema, dataSources });
+const port = Number.parseInt(process.env.PORT) || 4000;
 
-server.listen().then(({ url }) => {
+server.listen(port).then(({ url }) => {
   console.log(`Server ready at ${url}`);
 });
